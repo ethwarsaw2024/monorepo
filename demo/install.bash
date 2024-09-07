ee1b7ee90e66bfadf143d01fb8c3678491f75117
Piotr Roslaniec <p.roslaniec@gmail.com>
2024-09-07 15:56:44 +0200

    wip

--- demo/install.bash
+++ demo/install.bash
@@ -3,6 +3,6 @@
 set -e
 
 (cd ../magic-js && yarn install && yarn build)
-#(cd ../magic-js/packages/wagmi-magic-connector && yarn install && yarn build)
+(cd ../magic-js/packages/wagmi-magic-connector && yarn install && yarn build)
 
 yarn install
